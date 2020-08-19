df9f89709883f2c02a449face8cc0d699eed5fe0
Tage-official <69791812+Tage-official@users.noreply.github.com>
2020-08-19 21:21:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snake2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
